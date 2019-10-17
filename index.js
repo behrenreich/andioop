a52fc789222dcc84a96ad1f84bb3ffaa1100d510
behrenreich <51254514+behrenreich@users.noreply.github.com>
2019-10-17 18:53:41 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+client.login('NjM0NTM3MTI1ODYxMTMwMjQw.Xaj-cQ.LeP2mL9PmajqMiMQt5JU0-pQ_V8
+');
+
+client.on('ready', () => console.log('I am ready!'));
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        return msg.reply('@𝓢𝓴𝓮𝓵𝓮𝓽𝓲𝓽𝓪𝓷#4847');
+    }
+});
