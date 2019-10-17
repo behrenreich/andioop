a5f8dbb6049926d79dc86f0a8fe87267465490a3
behrenreich <51254514+behrenreich@users.noreply.github.com>
2019-10-17 18:55:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.login('NjM0NTM3MTI1ODYxMTMwMjQw.Xaj-cQ.LeP2mL9PmajqMiMQt5JU0-pQ_V8
 client.on('ready', () => console.log('I am ready!'));
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
+    if (msg.content === '@𝓢𝓴𝓮𝓵𝓮𝓽𝓲𝓽𝓪𝓷#4847') {
         return msg.reply('@𝓢𝓴𝓮𝓵𝓮𝓽𝓲𝓽𝓪𝓷#4847');
     }
 });
